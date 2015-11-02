ceef8a50431b803c470e1441c697a663b39af275
Info Mgt <apinfomgt@gmail.com>
2015-11-02 14:34:38 -0500

    Testing header read from Flask

--- slash-command.py
+++ slash-command.py
@@ -12,9 +12,9 @@ def reply():
     user_name = request.args.get('user_name')
     sc = SlackClient(os.environ['slack_token'])
     join_channel = sc.api_call('channels.join', name=text)
-    test_text = "test text"
+    testing = "Additional tests"
 
-    return test_text #'You just created a story called ' + text + ' your new slack channel is: #' + text
+    return testing #'You just created a story called ' + text + ' your new slack channel is: #' + text
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
