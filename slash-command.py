ffabaa285140608c6607a4617b0161b0460af4c5
Info Mgt <apinfomgt@gmail.com>
2015-11-02 14:06:48 -0500

    Continued testing for channel join and channel invite

--- slash-command.py
+++ slash-command.py
@@ -9,11 +9,12 @@ def reply():
     text = request.args.get('text')
     channel = request.args.get('channel_name')
     user_id = request.args.get('user_id')
+    headers = request.headers.get()
     user_name = request.args.get('user_name')
     sc = SlackClient(os.environ['slack_token'])
     join_channel = sc.api_call('channels.join', name=text)
-    channel_find = sc.api_call('channels.find', '#' + text)
-    return channel_find #'You just created a story called ' + text + ' your new slack channel is: #' + text
+
+    return headers #'You just created a story called ' + text + ' your new slack channel is: #' + text
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
