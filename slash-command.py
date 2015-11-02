3b04aebf246d2d86f47805fe0db29e1baa5a8ee0
Info Mgt <apinfomgt@gmail.com>
2015-11-02 15:07:14 -0500

    Testing header read from Flask

--- slash-command.py
+++ slash-command.py
@@ -14,7 +14,7 @@ def reply():
     join_channel = sc.api_call('channels.join', name=text)
     join_response = json.dumps(join_channel)
     read_response = json.loads(join_response)
-    channel_id = read_response["channel"]["id"]
+    channel_id = read_response["id"]
     return channel_id #'You just created a story called ' + text + ' your new slack channel is: #' + text
 
 if __name__ == "__main__":
