723703a1cfb6eb78879b98092488a398eafe1088
David Fox <dvdfox@gmail.com>
2015-11-05 17:32:03 -0500

    adding new flask app, updating Procfile

---
+++ eventsAPI.py
@@ -0,0 +1,13 @@
+from flask import request
+
+@app.route('/events', methods=['POST'])
+def create_request():
+    print(request.json)
+    return jsonify({'result': True})
+
+@app.route('/events', methods=['HEAD'])
+def head():
+    return jsonify({'result': True})
+
+if __name__ == '__main__':
+    app.run(debug=True)
