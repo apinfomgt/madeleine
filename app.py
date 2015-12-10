6e4285911f4a5dfa5fcec403a4554e9013d44adc
Info Mgt <apinfomgt@gmail.com>
2015-12-10 16:14:59 -0500

    Added import of slack.py to app.py and worked on passing the values recevied by slack.

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import json, os
 from slackclient import SlackClient
 from flask import Flask, request
+from slack import slack
 
 app = Flask(__name__)
 
 @app.route("/slack/events", methods = ['GET'])
-def reply():
+def slack_get():
     text = request.args.get('text')
     channel = request.args.get('channel_name')
     user_id = request.args.get('user_id')
     user_name = request.args.get('user_name')
+    return slack(text, channel, user_id, user_name)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- slack.py
+++ slack.py
@@ -1,10 +1,7 @@
 import json, os
 from slackclient import SlackClient
-from flask import Flask, request
 
-app = Flask(__name__)
-
-def slack():
+def slack(text, channel, user_id, user_name):
     sc = SlackClient(os.environ['slack_token'])
     join_channel = sc.api_call('channels.join', name=text)
     read_response = json.loads(join_channel)
@@ -13,6 +10,5 @@ def slack():
     post_text = 'A new event channel called: ' + text + ' has been created. Join to contribute.'
     return 'You just created a story called ' + text + ' your new slack channel is: #' + text + ' You have been invited to this channel.'
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     slack()
