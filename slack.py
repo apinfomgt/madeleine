96e0e4d2f42070726c18cae4cb20a13eebf49081
Info Mgt <apinfomgt@gmail.com>
2015-12-10 16:22:35 -0500

    Testing app.py to slack.py communication.

--- slack.py
+++ slack.py
@@ -8,7 +8,7 @@ def slack(text, channel, user_id, user_name):
     channel_id = read_response['channel']['id']
     invite_to_channel = sc.api_call('channels.invite', channel=channel_id, user=user_id)
     post_text = 'A new event channel called: ' + text + ' has been created. Join to contribute.'
-    return 'You just created a story called ' + text + ' your new slack channel is: #' + text + ' You have been invited to this channel.'
+    return 'Zach testing: You just created a story called ' + text + ' your new slack channel is: #' + text + ' You have been invited to this channel.'
 
 # if __name__ == "__main__":
 #     slack()
