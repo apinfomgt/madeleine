36b97eb7656366d60d14a8b3b4e04f95049c40d2
Info Mgt <apinfomgt@gmail.com>
2015-12-11 12:26:10 -0500

    Fixing app.py to communicate properly with slack.py

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ def slack_get():
     channel = request.args.get('channel_name')
     user_id = request.args.get('user_id')
     user_name = request.args.get('user_name')
-    slack(text, channel, user_id, user_name)
-    return ()
+    return slack(text, channel, user_id, user_name)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- slack.py
+++ slack.py
@@ -10,5 +10,5 @@ def slack(text, channel, user_id, user_name):
     post_text = 'A new event channel called: ' + text + ' has been created. Join to contribute.'
     return 'You just created a story called ' + text + ' your new slack channel is: #' + text + ' You have been invited to this channel.'
 
-# if __name__ == "__main__":
-#     slack()
+if __name__ == "__main__":
+    slack()
