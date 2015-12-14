fb6632c7a2fecaab77c486e63f86d0f1281a25e6
Info Mgt <apinfomgt@gmail.com>
2015-12-14 15:04:06 -0500

    Testing

--- slackapp.py
+++ slackapp.py
@@ -4,8 +4,8 @@ from slackclient import SlackClient
 # def slackcreate(text, channel, user_id, user_name):
 def slackcreate(text):
     sc = SlackClient(os.environ['slack_token'])
-    join_channel = sc.api_call('channels.join', name=text)
-    return join_channel
+    # join_channel = sc.api_call('channels.join', name=text)
+    return sc
     # read_response = json.loads(join_channel)
     # channel_id = read_response['channel']['id']
     # invite_to_channel = sc.api_call('channels.invite', channel=channel_id, user=user_id)
