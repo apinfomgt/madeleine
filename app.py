4f0a4519b162217c453fa5ee85997d64d2fddfda
Info Mgt <apinfomgt@gmail.com>
2015-12-14 15:01:18 -0500

    Testing

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def slack_get():
     channel = request.args.get('channel_name')
     user_id = request.args.get('user_id')
     user_name = request.args.get('user_name')
-    return text + channel + user_id + user_name # slackcreate(text, channel, user_id, user_name)
+    return slackcreate(text) # slackcreate(text, channel, user_id, user_name)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- slackapp.py
+++ slackapp.py
@@ -3,13 +3,14 @@ from slackclient import SlackClient
 
 # def slackcreate(text, channel, user_id, user_name):
 def slackcreate(text):
-    # sc = SlackClient(os.environ['slack_token'])
-    # join_channel = sc.api_call('channels.join', name=text)
+    sc = SlackClient(os.environ['slack_token'])
+    join_channel = sc.api_call('channels.join', name=text)
+    return join_channel
     # read_response = json.loads(join_channel)
     # channel_id = read_response['channel']['id']
     # invite_to_channel = sc.api_call('channels.invite', channel=channel_id, user=user_id)
-    post_text = 'A new event channel called: ' + text + ' has been created. Join to contribute.'
-    return 'Test - You just created a story called ' + text + ' your new slack channel is: #' + text + ' You have been invited to this channel.'
+    # post_text = 'A new event channel called: ' + text + ' has been created. Join to contribute.'
+    # return 'Test - You just created a story called ' + text + ' your new slack channel is: #' + text + ' You have been invited to this channel.'
 
 if __name__ == "__main__":
     slackcreate()
