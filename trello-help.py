4f213cb18301ebc73b9e3d455d7fadad94a16568
Info Management <apinfomgt@gmail.com>
2015-12-17 17:18:12 -0500

    testing help

---
+++ trello-help.py
@@ -0,0 +1,47 @@
+import os, re
+from trello import TrelloClient
+
+api_key = os.environ['TRELLO_API_KEY']
+api_secret = os.environ['TRELLO_API_SECRET']
+token = os.environ['TRELLO_TOKEN_KEY']
+token_secret = os.environ['TRELLO_TOKEN_SECRET']
+pub_board = os.environ['PUBLISH_BOARD']
+event_list = os.environ['EVENT_LIST']
+
+_trello_client = None
+
+def get_trello_client(api_key, api_secret, token, token_secret):
+    global _trello_client
+    if _trello_client == None:
+        _trello_client = TrelloClient(api_key=api_key,
+                api_secret=api_secret,
+                token=token,
+                token_secret=token_secret)
+    return _trello_client
+
+class MyTrelloClient(object):
+    def __init__(self):
+        self._trello = get_trello_client(api_key=api_key,
+                api_secret=api_secret,
+                token=token,
+                token_secret=token_secret)
+
+    def _list_boards(self):
+        boards = self._trello.list_boards()
+        return boards
+
+    def _get_board(self,id):
+        board = self._trello.get_board(id)
+        return board
+
+    def _add_board(self,name):
+        board = self._trello.add_board(name)
+        return board
+
+    def _get_card(self,id):
+        card = self._trello.get_card(id)
+        return card
+
+if __name__ == '__main__':
+    pubboard = MyTrelloClient()._get_board('5627a1f767216d6f4ded4816')
+    print pubboard.name
