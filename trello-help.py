45b65671e4d793e1c2b744db4df70b0424c9dbde
Info Management <apinfomgt@gmail.com>
2015-12-17 21:02:34 -0500

    help

--- trello-help.py
+++ trello-help.py
@@ -4,19 +4,19 @@ import requests
 
 requests.packages.urllib3.disable_warnings
 
-#api_key = os.environ['TRELLO_API_KEY']
-#api_secret = os.environ['TRELLO_API_SECRET']
-#token = os.environ['TRELLO_TOKEN_KEY']
-#token_secret = os.environ['TRELLO_TOKEN_SECRET']
-#pub_board = os.environ['PUBLISH_BOARD']
-#event_list = os.environ['EVENT_LIST']
+api_key = os.environ['TRELLO_API_KEY']
+api_secret = os.environ['TRELLO_API_SECRET']
+token = os.environ['TRELLO_TOKEN_KEY']
+token_secret = os.environ['TRELLO_TOKEN_SECRET']
+pub_board = os.environ['PUBLISH_BOARD']
+event_list = os.environ['EVENT_LIST']
 
-api_key='d5ce3f1198288ff5de75d0f8c13ecd0c'
-api_secret='59546674a99f6287cbc49259beac752ef3d0481425f1f0b2fca8c1b3a46843a7'
-token='a765d04f047650757ed7ccac107a72bbd5562d50a320c9605228eed698f2b35d'
-token_secret='5c9415d771eb90137b939ac77fa394cb'
-pub_board='5633d93ffc0721b51330a0e1'
-event_list='5633d94a2460148854315431'
+#api_key='d5ce3f1198288ff5de75d0f8c13ecd0c'
+#api_secret='59546674a99f6287cbc49259beac752ef3d0481425f1f0b2fca8c1b3a46843a7'
+#token='a765d04f047650757ed7ccac107a72bbd5562d50a320c9605228eed698f2b35d'
+#token_secret='5c9415d771eb90137b939ac77fa394cb'
+#pub_board='5633d93ffc0721b51330a0e1'
+#event_list='5633d94a2460148854315431'
 
 _trello_client = None
 
@@ -54,7 +54,12 @@ class MyTrelloClient(object):
 
 if __name__ == '__main__':
     print api_key
+	print api_secret
     print token
+	print token_secret
+	print pub_board
+	print event_list
+	
     boards = MyTrelloClient()._list_boards()
     for b in boards:
         print b.name
