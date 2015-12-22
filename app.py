518b97e1f8cac544a2414b2db3c848944df79520
Info Mgt <apinfomgt@gmail.com>
2015-12-22 15:59:41 -0500

    Testing

--- app.py
+++ app.py
@@ -20,43 +20,6 @@ def slack_get():
     eventid = get_uuid()
     return slackcreate(text, channel, user_id, user_name, eventid)
 
-'''
-@app.route('/trello/events', methods=['POST'])
-def trello_new_event():
-    try:
-        response = request.data
-        data = json.loads(response)
-        print data
-        cardid = data['action']['data']['card']['id']
-        print cardid
-        name = data['action']['data']['card']['name']
-        print name
-        try:
-            description = data['action']['data']['card']['desc']
-        except:
-            description = 'Enter description'
-        print description
-        #update this with uuid function when we have it
-        guid = get_uuid()
-        print 'creating new board'
-        newboard = TrelloCreate()._create_event_board(name=name,guid=guid,description=description)
-        print newboard
-        url = newboard.url
-        print url
-        card = MyTrelloClient()._get_card(cardid)
-        print card
-        TrelloCreate()._update_event_card(newboard,card)
-        return jsonify({'result': True})
-    except Exception as e:
-        print(str(e))
-        return jsonify({'result': 'Error'})
-
-    return jsonify({'result': True})
-
-@app.route('/trello/events', methods=['HEAD'])
-def head():
-    return jsonify({'result': True})
-'''
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
