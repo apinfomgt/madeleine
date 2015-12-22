c0b0f7df679964389f5f845b86f86f7d331f0af3
Info Management <apinfomgt@gmail.com>
2015-12-22 11:07:58 -0500

    help me

--- trello-test-env-variable.py
+++ trello-test-env-variable.py
@@ -4,9 +4,13 @@ import requests
 
 requests.packages.urllib3.disable_warnings
 
-api_key = str(os.environ['TRELLO_API_KEY'])
-token = str(os.environ['TRELLO_TOKEN_KEY'])
-boardid = str(os.environ['PUBLISH_BOARD'])
+api_key = os.environ['TRELLO_API_KEY']
+token = os.environ['TRELLO_TOKEN_KEY']
+boardid = os.environ['PUBLISH_BOARD']
+
+#api_key='d5ce3f1198288ff5de75d0f8c13ecd0c'
+#token='a765d04f047650757ed7ccac107a72bbd5562d50a320c9605228eed698f2b35d'
+#boardid='5633d93ffc0721b51330a0e1'
 
 
 if __name__ == '__main__':
@@ -15,6 +19,7 @@ if __name__ == '__main__':
     print boardid
 
     _trello = TrelloClient(api_key=api_key,token=token)
+    print _trello
     try:
         board = _trello.get_board(boardid)
         print board.name
@@ -24,8 +29,7 @@ if __name__ == '__main__':
     try:
         url = 'https://trello.com/1/boards/' + boardid + '?key=' + api_key + '&token=' + token
         print url
-	r = requests.get(url)
-	print r
+        r = requests.get(url)
         print json.loads(r.text)['name']
     except:
         print 'failed via requests'
